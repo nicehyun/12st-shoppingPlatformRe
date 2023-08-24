af211096009d3b3d53ec22ec8dfaef5784a612f4
ish <lshsh5656@naver.com>
2023-08-24 23:01:05 +0900

    TODO 제거

--- src/features/auth/signUp/views/mobile/MSignUpEmailInput.tsx
+++ src/features/auth/signUp/views/mobile/MSignUpEmailInput.tsx
@@ -1,7 +1,6 @@
 import SignUpEmailInput from "../SignUpEmailInput"
 import MSignUpInputLayout from "./MSignUpInputLayout"
 
-// TODO : isEmailValid 글로벌로 변경하기
 const MSignUpEmailInput = () => {
   return (
     <MSignUpInputLayout headingText="로그인에 사용할 이메일을 입력해주세요">
--- src/features/auth/signUp/views/mobile/MSignUpPasswordInput.tsx
+++ src/features/auth/signUp/views/mobile/MSignUpPasswordInput.tsx
@@ -1,7 +1,6 @@
 import SignUpPasswordInput from "../SignUpPasswordInput"
 import MSignUpInputLayout from "./MSignUpInputLayout"
 
-// TODO : isPasswordValid 글로벌로 변경하기
 const MSignUpPasswordInput = () => {
   return (
     <MSignUpInputLayout headingText="로그인에 사용할 비밀번호를 입력해주세요">
