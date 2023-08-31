f95628571da10b001d9045ea5d00d813d15e49e8
ish <lshsh5656@naver.com>
2023-08-31 10:49:27 +0900

    Design : 장바구니 버튼 디자인 수정

--- src/features/layout/views/HeaderCartButton.tsx
+++ src/features/layout/views/HeaderCartButton.tsx
@@ -1,11 +1,6 @@
 "use client"
 
-import useSessionQuery from "@/app/api/user/[email]/useSessionQuery"
 import { useProductListInCartQuery } from "@/features/cart/hooks/useProductListInCartQuery"
-import { getProductListInCart } from "@/firebase/firestore/cart"
-import { useAppDispatch } from "@/redux/hooks"
-import { useSession } from "next-auth/react"
-import { useEffect, useState } from "react"
 import { BsFillCartXFill, BsFillCartFill } from "react-icons/bs"
 
 const HeaderCartButton = () => {
@@ -13,7 +8,7 @@ const HeaderCartButton = () => {
 
   return (
     <button className="group mx-[10px] border border-border rounded-lg px-2 py-[4px] flex hover:bg-black hover:text-lightRed dark:hover:bg-white dark:hover:text-lightRed transition-3 flexCenter peer">
-      <span>
+      <span className="mr-[5px]">
         {productListInCart.length < 10 ? (
           <BsFillCartFill className="cart-el cart-icon" />
         ) : (
@@ -22,7 +17,7 @@ const HeaderCartButton = () => {
       </span>
       <span className="text-[10px]">SHOPPING CART</span>
 
-      <span className="border border-border rounded-lg text-[8px] ml-[8px] p-[1px] w-25 h-18 font-bold bg-white text-black group-hover:text-lightRed">
+      <span className="border border-border rounded-lg text-[8px] ml-[8px] py-[1px] px-[3px] font-bold bg-white text-black group-hover:text-lightRed">
         {productListInCart.length}
       </span>
     </button>
