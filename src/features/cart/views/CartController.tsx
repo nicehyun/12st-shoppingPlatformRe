1f2adc77afc97799be5499ab1015856dd62ee90c
ish <lshsh5656@naver.com>
2023-09-05 11:02:41 +0900

    Design : 반응형

--- src/features/cart/views/CartController.tsx
+++ src/features/cart/views/CartController.tsx
@@ -6,13 +6,13 @@ const CartController = () => {
     <div className="mt-[40px] lg:mt-[60px] xl:mt-[60px] flexCenter">
       <button
         onClick={() => routeTo(ROUTE.HOME)}
-        className="w-[320px] h-[60px] border-[1px] border-black dark:border-white mr-[12px] text-[12px] font-bold hover:text-lightRed"
+        className="w-[320px] h-[80px] sm:h-[60px] md:h-[70px] border-[1px] border-black dark:border-white mr-[12px] text-[20px] font-bold hover:text-lightRed"
       >
         CONTINUE SHOPPING
       </button>
       <button
         onClick={() => routeTo(ROUTE.CHECKOUT)}
-        className="w-[320px] h-[60px] border-[1px] border-black dark:border-white text-white dark:text-black bg-black dark:bg-white text-[12px] font-bold hover:text-lightRed dark:hover:text-lightRed"
+        className="w-[320px] h-[80px] sm:h-[60px] md:h-[70px] border-[1px] border-black dark:border-white text-white dark:text-black bg-black dark:bg-white text-[20px] font-bold hover:text-lightRed dark:hover:text-lightRed"
       >
         CHECK OUT
       </button>
