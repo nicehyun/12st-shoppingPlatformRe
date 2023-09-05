cc0c0d4b6c0a67b52a7d2a5e5e0ad576d4ca97c6
ish <lshsh5656@naver.com>
2023-09-05 10:34:09 +0900

    Design : 반응형

--- src/features/cart/views/CouponBar.tsx
+++ src/features/cart/views/CouponBar.tsx
@@ -6,19 +6,28 @@ import InputLabel from "@mui/material/InputLabel"
 import MenuItem from "@mui/material/MenuItem"
 import useCouponQuery from "../hooks/useCouponQuery"
 import { useAppDispatch, useAppSelector } from "@/redux/hooks"
-import { selectCoupon, selectSeletedCoupon } from "@/redux/features/couponSlice"
+import {
+  resetCoupon,
+  selectCoupon,
+  selectSeletedCoupon,
+} from "@/redux/features/couponSlice"
 import { SelectChangeEvent } from "@mui/material"
-
-// TODO : 선택 상품 총 가격 15000원 이상일 경우 쿠폰 사용가능, 15000원 이하 쿠폰 disabled 처리
+import {
+  accumulationOfProductsPrice,
+  priceToUseCoupon,
+} from "@/common/utils/price"
+import { useProductListInCartQuery } from "../hooks/useProductListInCartQuery"
+import { selectCheckedProductList } from "@/redux/features/cartSlice"
+import { useEffect } from "react"
 
 const CouponBar = () => {
   const { coupons } = useCouponQuery()
-  console.log(coupons)
 
   const dispatch = useAppDispatch()
 
+  const { productListInCart } = useProductListInCartQuery()
   const seletedCoupon = useAppSelector(selectSeletedCoupon)
-  console.log(seletedCoupon)
+  const checkedProductList = useAppSelector(selectCheckedProductList)
 
   const handleCouponSelect = (event: SelectChangeEvent<unknown>) => {
     if (coupons) {
@@ -30,13 +39,28 @@ const CouponBar = () => {
     }
   }
 
-  return (
-    <div className="sticky lg:top-[150px] md:top-[0px] xl:top-[150px] w-[190px] sm:w-[140px] md:w-[160px] h-[400px] sm:h-[340px] md:h-[370px] py-[20px] p-[10px] border-[1px] border-black rounded-[5px] shadow bg-white">
-      <h4 className="mb-[32px] sm:mb-[14px] text-black">쿠폰</h4>
+  const totalPriceOfCheckedProduct =
+    productListInCart &&
+    accumulationOfProductsPrice(
+      productListInCart?.filter((product) =>
+        checkedProductList.includes(product.id)
+      )
+    )
 
+  const discountedPriceWithCoupon =
+    seletedCoupon && totalPriceOfCheckedProduct
+      ? priceToUseCoupon(seletedCoupon, totalPriceOfCheckedProduct)
+      : 0
+
+  useEffect(() => {
+    dispatch(resetCoupon())
+  }, [checkedProductList])
+
+  return (
+    <div className="sticky lg:top-[150px] md:top-[0px] xl:top-[150px] w-[190px] sm:w-[140px] md:w-[160px] h-[350px] sm:h-[270px] md:h-[300px] py-[20px] p-[10px] border-[1px] border-black rounded-[5px] shadow bg-white">
       <div className="mb-[30px] sm:mb-[20px]">
         <p className="text-[14px] text-lightBlack mb-[10px] sm:mb-[4px]">
-          보너스 쿠폰
+          쿠폰
         </p>
         <FormControl
           variant="standard"
@@ -49,7 +73,11 @@ const CouponBar = () => {
             value={seletedCoupon?.name ?? ""}
             label="Coupon"
             onChange={handleCouponSelect}
-            // disabled={isCouponDisabled}
+            disabled={
+              totalPriceOfCheckedProduct
+                ? totalPriceOfCheckedProduct < 15000
+                : false
+            }
             className="text-[14px]"
           >
             <MenuItem
@@ -71,27 +99,12 @@ const CouponBar = () => {
         </FormControl>
       </div>
 
-      <div className="mb-[30px] sm:mb-[20px]">
-        <p className="text-[14px] text-lightBlack mb-[10px] md:text-[12px] sm:mb-[4px]">
-          브랜드 쿠폰
-        </p>
-        <FormControl
-          variant="standard"
-          className="min-w-[170px] sm:min-w-[120px] md:min-w-[140px]"
-        >
-          <InputLabel className="text-[12px]">Coupon</InputLabel>
-          <Select label="Coupon" value="" disabled>
-            <MenuItem value="">선택안함</MenuItem>
-          </Select>
-        </FormControl>
-      </div>
-
       <div className="absolute bottom-[10px] m-auto flex flex-col">
         <span className="text-[14px] text-black">쿠폰 사용금액</span>
 
         <div className="text-[16px] sm:text-[14px] md:text-[14px] font-bold text-lightRed mr-[3px]">
           {/* <span>{priceToUseCoupon}</span> <span>원</span> */}
-          <span>16000</span> <span>원</span>
+          <span>{discountedPriceWithCoupon}</span> <span>원</span>
         </div>
       </div>
     </div>
