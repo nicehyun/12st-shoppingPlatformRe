925f994156f41580128fdd3c3b0777f5463955ae
ish <lshsh5656@naver.com>
2023-09-05 11:38:54 +0900

    Feat : checkout disabled 설정

--- src/features/cart/views/CartController.tsx
+++ src/features/cart/views/CartController.tsx
@@ -1,7 +1,16 @@
 import { ROUTE, useNavigations } from "@/common/hooks/useNavigations"
+import { selectCheckedProductList } from "@/redux/features/cartSlice"
+import { useAppSelector } from "@/redux/hooks"
 
 const CartController = () => {
   const { routeTo } = useNavigations()
+  const checkedProductList = useAppSelector(selectCheckedProductList)
+
+  const handleCheckout = () => {
+    if (!checkedProductList.length) return
+
+    routeTo(ROUTE.CHECKOUT)
+  }
   return (
     <div className="mt-[40px] lg:mt-[60px] xl:mt-[60px] flexCenter">
       <button
@@ -11,8 +20,9 @@ const CartController = () => {
         CONTINUE SHOPPING
       </button>
       <button
-        onClick={() => routeTo(ROUTE.CHECKOUT)}
-        className="w-[320px] h-[80px] sm:h-[60px] md:h-[70px] border-[1px] border-black dark:border-white text-white dark:text-black bg-black dark:bg-white text-[20px] font-bold hover:text-lightRed dark:hover:text-lightRed"
+        onClick={handleCheckout}
+        disabled={!checkedProductList.length}
+        className="w-[320px] h-[80px] sm:h-[60px] md:h-[70px] border-[1px] border-black dark:border-white text-white dark:text-black bg-black dark:bg-white text-[20px] font-bold hover:text-lightRed dark:hover:text-lightRed disabled:cursor-not-allowed disabled:bg-border disabled:text-white dark:disabled:text-black"
       >
         CHECK OUT
       </button>
