a0929c11d969ecb32ef7d3631dcbf3f9f3e4e94b
ish <lshsh5656@naver.com>
2023-09-13 00:00:54 +0900

    Feat : 요청사항 직접입력 선택시 textarea 마운트

--- src/features/checkout/CheckoutInputLayout.tsx
+++ src/features/checkout/CheckoutInputLayout.tsx
@@ -30,13 +30,13 @@ const CheckoutInputLayout = ({
   return (
     <>
       <div
-        className={`flex items-center mb-[${
+        className={`flex items-start mb-[${
           inputState?.hasError ? "0px" : "20px"
         }] mr-0`}
       >
         <label
           htmlFor={id}
-          className="min-w-[100px] sm:text-[12px] md:text-[14px]"
+          className="min-w-[100px] sm:text-[12px] md:text-[14px] sm:pt-[6px] md:pt-[8px] pt-[11px]"
         >
           {label} {isRequired && <span className="text-lightRed">*</span>}
         </label>
--- src/features/checkout/DeliveryMemoSelect.tsx
+++ src/features/checkout/DeliveryMemoSelect.tsx
@@ -34,55 +34,67 @@ const DeliveryMemoSelect = () => {
     handleClose()
   }
   return (
-    <Select
-      open={isOpen}
-      onClose={handleClose}
-      onOpen={handleOpen}
-      displayEmpty
-      value={selectedMemo}
-      onChange={handleSelectChange}
-      input={<OutlinedInput />}
-      renderValue={(selected) => {
-        if (selected.length === 0) {
-          return "배송시 요청사항을 선택해주세요"
-        }
+    <div className="w-full max-w-[500px]">
+      <Select
+        open={isOpen}
+        onClose={handleClose}
+        onOpen={handleOpen}
+        displayEmpty
+        value={selectedMemo}
+        onChange={handleSelectChange}
+        input={<OutlinedInput />}
+        renderValue={(selected) => {
+          if (selected.length === 0) {
+            return "배송시 요청사항을 선택해주세요"
+          }
 
-        return selected
-      }}
-      MenuProps={{
-        PaperProps: {
-          style: {
-            maxHeight: 48 * 4.5 + 8,
-            width: "200px",
+          return selected
+        }}
+        MenuProps={{
+          PaperProps: {
+            style: {
+              maxHeight: 48 * 4.5 + 8,
+              width: "200px",
+            },
           },
-        },
-      }}
-      // inputProps={{ "aria-label": "Without label" }}
-      sx={{
-        width: "500px",
-        height: "50px",
-        fontSize: "16px",
-        border: "1px solid rgb(180, 180, 180)",
+        }}
+        sx={{
+          width: "500px",
+          height: "50px",
+          fontSize: "16px",
+          border: "1px solid rgb(180, 180, 180)",
 
-        "& .MuiOutlinedInput-notchedOutline": {
-          border: "rgb(180, 180, 180)",
-        },
-      }}
-      className="h-[50px] sm:h-[40px] md:h-[44px] sm:text-[12px] md:text-[14px] dark:text-white"
-    >
-      <MenuItem disabled value="" className="sm:text-[12px] md:text-[14px]">
-        배송시 요청사항을 선택해주세요
-      </MenuItem>
-      {memos.map((memo, index) => (
-        <MenuItem
-          key={`deliveyMemo-${index}`}
-          value={memo}
-          className="sm:text-[12px] md:text-[14px]"
-        >
-          {memo}
+          "& .MuiOutlinedInput-notchedOutline": {
+            border: "rgb(180, 180, 180)",
+          },
+        }}
+        className={`h-[50px] sm:h-[40px] md:h-[44px] sm:text-[12px] md:text-[14px] dark:text-white w-full`}
+      >
+        <MenuItem disabled value="" className="sm:text-[12px] md:text-[14px]">
+          배송시 요청사항을 선택해주세요
         </MenuItem>
-      ))}
-    </Select>
+        {memos.map((memo, index) => (
+          <MenuItem
+            key={`deliveyMemo-${index}`}
+            value={memo}
+            className="sm:text-[12px] md:text-[14px]"
+          >
+            {memo}
+          </MenuItem>
+        ))}
+      </Select>
+      {/* TODO api에서 selectedMemo=== "직접입력일경우 deliveryMemo-direct 가져오기" */}
+      {selectedMemo === "직접입력" && (
+        <textarea
+          id="deliveryMemo-direct"
+          cols={30}
+          rows={5}
+          maxLength={50}
+          placeholder="요청사항을 입력해주세요. (최대 50자)"
+          className="w-full h-[150px] overflow-auto py-[19px] px-[14px] border-inputBorder mt-[10px] text-[14px] sm:text-[12px] leading-[20px] appearance-none resize-none"
+        ></textarea>
+      )}
+    </div>
   )
 }
 
