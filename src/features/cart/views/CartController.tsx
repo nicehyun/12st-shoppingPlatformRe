e8bdb6e99662d848780c535869d2a5dce2caa69f
ish <lshsh5656@naver.com>
2023-09-25 23:32:43 +0900

    Design : 반응형

--- src/features/cart/views/CartController.tsx
+++ src/features/cart/views/CartController.tsx
@@ -15,14 +15,14 @@ const CartController = () => {
     <section className="mt-[120px] flexCenter">
       <button
         onClick={() => routeTo(ROUTE.HOME)}
-        className="w-[320px] h-[80px] sm:h-[60px] md:h-[70px] border-[1px] border-black dark:border-white mr-[12px] text-[20px] font-bold hover:text-lightRed"
+        className="w-[400px] h-[80px] sm:h-[60px] md:h-[70px] border-[1px] border-black dark:border-white mr-[12px] text-[28px] md:text-[24px] sm:text-[20px] font-bold hover:text-lightRed"
       >
         CONTINUE SHOPPING
       </button>
       <button
         onClick={handleCheckout}
         disabled={!checkedProductList.length}
-        className="w-[320px] h-[80px] sm:h-[60px] md:h-[70px] border-[1px] border-black dark:border-white text-white dark:text-black bg-black dark:bg-white text-[20px] font-bold hover:text-lightRed dark:hover:text-lightRed disabled:cursor-not-allowed disabled:bg-border dark:disabled:bg-border disabled:text-white dark:disabled:text-black"
+        className="w-[400px] h-[80px] sm:h-[60px] md:h-[70px] border-[1px] border-black dark:border-white text-white dark:text-black bg-black dark:bg-white text-[28px] md:text-[24px] sm:text-[20px] font-bold hover:text-lightRed dark:hover:text-lightRed disabled:cursor-not-allowed disabled:bg-border dark:disabled:bg-border disabled:text-white dark:disabled:text-black"
       >
         CHECK OUT
       </button>
