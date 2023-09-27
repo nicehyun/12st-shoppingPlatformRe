ae85328cdacad82134c7f1448e1cb4b54e05cf47
ish <lshsh5656@naver.com>
2023-09-28 01:26:12 +0900

    Feat : 등록된 기본 주소 없을 경우 버튼 클릭 후 양식 마운트

--- src/features/checkout/views/CheckoutDefalutDeliveryInfo.tsx
+++ src/features/checkout/views/CheckoutDefalutDeliveryInfo.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { useGetAddressQuery } from "../hooks/useGetAddressQuery"
 import CheckoutAddressInput from "./CheckoutAddressInput"
 import CheckoutDeliveryNameInput from "./CheckoutDeliveryNameInput"
@@ -7,32 +8,47 @@ import DeliveryMemoSelect from "./DeliveryMemoSelect"
 
 const CheckoutDefalutDeliveryInfo = () => {
   const { userDefalutAddress } = useGetAddressQuery()
+  const [isShowDeliveryInfo, setIsShowDeliveryInfo] = useState(false)
 
   return (
     <>
-      <CheckoutDeliveryNameInput
-        defaultValue={userDefalutAddress?.delivertName}
-      />
-      <CheckoutRecipientInput defaultValue={userDefalutAddress?.recipient} />
-      <CheckoutAddressInput
-        defaultValue={{
-          zipcode: userDefalutAddress?.zipcode,
-          address: userDefalutAddress?.address,
-          additionalAddress: userDefalutAddress?.additionalAddress,
-        }}
-      />
-      <CheckoutPhoneInput
-        isRequired
-        defaultValue={userDefalutAddress?.phone1}
-      />
-      <CheckoutPhoneInput defaultValue={userDefalutAddress?.phone2} />
-
-      <p className="max-w-[500px] ml-[100px] text-[14px] sm:text-[12px] mb-[20px] font-semibold">
-        * 기본 배송지입니다. 주문 시 변경하신 내용으로 기본 배송지 주소가
-        수정됩니다.
-      </p>
-
-      <DeliveryMemoSelect />
+      {isShowDeliveryInfo || userDefalutAddress ? (
+        <>
+          <CheckoutDeliveryNameInput
+            defaultValue={userDefalutAddress?.delivertName}
+          />
+          <CheckoutRecipientInput
+            defaultValue={userDefalutAddress?.recipient}
+          />
+          <CheckoutAddressInput
+            defaultValue={{
+              zipcode: userDefalutAddress?.zipcode,
+              address: userDefalutAddress?.address,
+              additionalAddress: userDefalutAddress?.additionalAddress,
+            }}
+          />
+          <CheckoutPhoneInput
+            isRequired
+            defaultValue={userDefalutAddress?.phone1}
+          />
+          <CheckoutPhoneInput defaultValue={userDefalutAddress?.phone2} />
+          <p className="max-w-[500px] ml-[100px] text-[14px] sm:text-[12px] mb-[20px] font-semibold">
+            * 기본 배송지입니다. 주문 시 변경하신 내용으로 기본 배송지 주소가
+            수정됩니다.
+          </p>
+          <DeliveryMemoSelect />
+        </>
+      ) : (
+        <div className="flexCenter h-[200px]">
+          <button
+            type="button"
+            onClick={() => setIsShowDeliveryInfo(true)}
+            className="border-[1px] border-black bg-black text-white text-[18px] md:text-[16px] sm:text-[14px] py-[18px] px-[10px]"
+          >
+            기본 배송지 추가하기
+          </button>
+        </div>
+      )}
     </>
   )
 }
