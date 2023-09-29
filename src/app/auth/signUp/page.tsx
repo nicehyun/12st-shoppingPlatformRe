89006a397cbf7be2566dfa0ed042cff221e27f8b
ish <lshsh5656@naver.com>
2023-09-30 01:09:25 +0900

    Feat : SignUp Page multiple layout set

--- src/app/auth/signUp/page.tsx
+++ src/app/auth/signUp/page.tsx
@@ -1,12 +1,12 @@
 import PageLayout from "@/common/views/PageLayout"
 import SignUpBasicModal from "@/features/auth/signUp/views/SignUpBasicModal"
 import SignUpForm from "@/features/auth/signUp/views/SignUpForm"
-
-//TODO : home으로 이동 후 회원가입 이동 시 activeStep 0으로 초기화 안됨
+import Header from "@/features/layout/views/Header"
 
 const SignUpPage = () => {
   return (
     <>
+      <Header isShowCart={false} />
       <PageLayout>
         <SignUpForm />
       </PageLayout>
