c066128040680b7be85fbbd212453de24fba888c
ish <lshsh5656@naver.com>
2023-10-04 23:36:23 +0900

    test

---
+++ src/app/myPage/page.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const MyPagePage = () => {
+  return <div>마이페이지</div>
+}
+
+export default MyPagePage
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,5 @@
 export { default } from "next-auth/middleware"
 
 export const config = {
-  matcher: [
-    "/cart/:path*",
-    "/checkout/:path*",
-    "/checkoutConfirmed/:path*",
-    "/myPage/:path*",
-  ],
+  matcher: ["/cart/:path*", "/checkout/:path*", "/checkoutConfirmed/:path*"],
 }
