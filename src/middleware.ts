94d6d4bd7004fb9b28dc382b0a096834624624e1
ish <lshsh5656@naver.com>
2023-10-04 22:13:51 +0900

    Feat : 로그인시에만 사용 가능 페이지 설정

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,10 @@
 export { default } from "next-auth/middleware"
 
 export const config = {
-  matcher: ["/cart/:path*", "/checkout/:path*", "/checkoutConfirmed/:path*"],
+  matcher: [
+    "/cart/:path*",
+    "/checkout/:path*",
+    "/checkoutConfirmed/:path*",
+    "/myPage/:path*",
+  ],
 }
