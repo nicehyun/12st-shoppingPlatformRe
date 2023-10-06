99dcdcc01efe4b444d48c53b69ce583748408ca5
ish <lshsh5656@naver.com>
2023-10-06 22:02:55 +0900

    Design : css 수정

--- src/features/cart/views/CartController.tsx
+++ src/features/cart/views/CartController.tsx
@@ -23,7 +23,7 @@ const CartController = () => {
       <Button
         onClick={handleCheckout}
         isDisabled={!checkedProductList.length}
-        classNames="w-[400px] h-[80px] sm:h-[60px] md:h-[70px] border-[1px] border-black dark:border-white text-white dark:text-black bg-black dark:bg-white text-[28px] md:text-[24px] sm:text-[20px] font-bold hover:text-lightRed dark:hover:text-lightRed disabled:cursor-not-allowed disabled:bg-border dark:disabled:bg-border disabled:text-white dark:disabled:text-black"
+        classNames="w-[400px] h-[80px] sm:h-[60px] md:h-[70px] border-[1px] border-black dark:border-white text-white dark:text-black bg-black dark:bg-white text-[28px] md:text-[24px] sm:text-[20px] font-bold hover:text-lightRed dark:hover:text-lightRed disabled:border-border disabled:cursor-not-allowed disabled:bg-border dark:disabled:bg-border disabled:text-white dark:disabled:text-black"
         content="CHECK OUT"
       />
     </section>
