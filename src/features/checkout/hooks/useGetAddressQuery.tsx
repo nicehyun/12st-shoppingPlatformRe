acaf911adad9959b0a74927de93476a7304c074c
ish <lshsh5656@naver.com>
2023-10-06 23:01:41 +0900

    Refactor : enabled 제거

--- src/features/checkout/hooks/useGetAddressQuery.tsx
+++ src/features/checkout/hooks/useGetAddressQuery.tsx
@@ -9,9 +9,6 @@ export const useGetAddressQuery = () => {
     ["userDefaultAddress"],
     () => getUserDefaultDeliveryInfo(sessionQuery?.user.email ?? ""),
     {
-      // TODO : AuthenticationProvider 로 세션 검사 시 enabled 옵션 제거하기
-      // enabled 옵션을 사용하여 sessionQuery 값이 설정된 이후에만 쿼리를 실행
-      enabled: !!sessionQuery,
       suspense: true,
     }
   )
