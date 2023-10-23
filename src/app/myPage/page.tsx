68bdce2995ac347f0bacb921d0d42ed12761a112
ish <lshsh5656@naver.com>
2023-10-23 17:27:25 +0900

    Feat : mypage

--- src/app/myPage/page.tsx
+++ src/app/myPage/page.tsx
@@ -1,13 +1,16 @@
 import PageLayout from "@/common/views/PageLayout"
 import Header from "@/features/layout/views/Header"
 import Navigation from "@/features/layout/views/Navigation"
+import MyPageLayout from "@/features/myPage/MyPageLayout"
 
 const MyPagePage = () => {
   return (
     <>
       <Header isShowCart={true} />
 
-      <PageLayout classNames="px-0">asd</PageLayout>
+      <PageLayout classNames="px-0">
+        <MyPageLayout />
+      </PageLayout>
 
       <Navigation />
     </>
---
+++ src/features/myPage/MyPageLayout.tsx
@@ -0,0 +1,10 @@
+const MyPageLayout = () => {
+  return (
+    <div className="flex">
+      <div className="bg-lightRed w-[200px] h-full mr-[20px]">sd</div>
+      <div className="bg-lightRed w-[400px] h-full">dss</div>
+    </div>
+  )
+}
+
+export default MyPageLayout
