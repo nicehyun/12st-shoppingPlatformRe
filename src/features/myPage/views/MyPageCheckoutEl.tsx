17ffcd55b1c04c5c72f3178961e7100a844b9af1
ish <lshsh5656@naver.com>
2023-10-25 01:25:20 +0900

    Design : group css 수정

--- src/features/myPage/views/MyPageCheckoutEl.tsx
+++ src/features/myPage/views/MyPageCheckoutEl.tsx
@@ -24,10 +24,10 @@ const MyPageCheckoutEl = ({ checkoutList, className }: IMyPageCheckoutEl) => {
       </div>
 
       <div
-        className={`flex justify-start w-1/2 cursor-pointer  group-hover:text-lightRed group-hover:underline`}
+        className={`flex justify-start w-1/2 cursor-pointer  group-hover:text-lightRed `}
       >
         <span
-          className={`block sm:h-[34.8px] md:h-[34.8px] sm:truncate-2 md:truncate-2 lg:truncate-2 xl:mr-[10px]`}
+          className={`block sm:h-[34.8px] md:h-[34.8px] sm:truncate-2 md:truncate-2 lg:truncate-2 xl:mr-[10px] group-hover:underline`}
         >
           {prductList[0].name}
         </span>
