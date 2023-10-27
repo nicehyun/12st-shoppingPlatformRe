eb61f24c49ee50f6bf90fec797b84893782eab73
ish <lshsh5656@naver.com>
2023-10-27 20:38:41 +0900

    Feat : marketing clause modal

--- src/features/myPage/views/userInfoOfMotification/MyPageClauseModification.tsx
+++ src/features/myPage/views/userInfoOfMotification/MyPageClauseModification.tsx
@@ -3,16 +3,29 @@
 import Button from "@/common/views/Button"
 import ClauseCheckbox from "@/common/views/ClauseCheckbox"
 import Loading from "@/common/views/Loading"
+import { showBasicModal } from "@/redux/features/modalSlice"
+import { useAppDispatch } from "@/redux/hooks"
 import { useEffect, useState } from "react"
 import useCheckMarketingClauseMutation from "../../hooks/useCheckMarketingClauseMutation"
 import { useGetUserInfoQuery } from "../../hooks/useGetUserInfoQuery"
 
 const MyPageClauseModification = () => {
+  const dispatch = useAppDispatch()
   const { mutateAsync, isLoading } = useCheckMarketingClauseMutation()
   const { userInfo } = useGetUserInfoQuery()
   const [isCheckedMarketingClause, setIsCheckedMarketingClause] =
     useState(false)
   const [isChagedClauseState, setIsChagedClauseState] = useState(false)
+
+  const handleMarketingClauseClick = () => {
+    dispatch(
+      showBasicModal({
+        modalId: "clause-marketing",
+        modalTitle: "광고성 정보 수신 및 마케팅 활용 동의",
+        modalContent: "MarketingClause",
+      })
+    )
+  }
 
   const handleMarketingClauseModification = async () => {
     if (userInfo?.marketingClause === isCheckedMarketingClause || isLoading)
@@ -43,7 +56,7 @@ const MyPageClauseModification = () => {
           borderColor: "peer-checked/marketing:after:border-lightRed",
         }}
         onClickClauseLabel={toggleMarketingClause}
-        onClickDetailClause={() => console.log(123)}
+        onClickDetailClause={handleMarketingClauseClick}
       />
       <button className="disabled:cursor-not-allowed disabled:bg-border "></button>
 
@@ -60,7 +73,7 @@ const MyPageClauseModification = () => {
           )
         }
         isDisabled={!isChagedClauseState}
-        classNames={`disabled:cursor-not-allowed disabled:bg-border disabled:text-white disabled:border-border mt-[30px] h-[50px] w-[400px] md:w-full sm:w-full sm:h-[40px] md:h-[44px] border-[1px] text-lightRed border-lightRed dark:bg-lightRed text-lightRed dark:text-black text-[14px] font-semibold rounded-[5px]`}
+        classNames={`mt-[30px] h-[50px] w-[400px] md:w-full sm:w-full sm:h-[40px] md:h-[44px] border-[1px] text-lightRed border-lightRed dark:bg-lightRed text-lightRed dark:text-black text-[14px] font-semibold rounded-[5px]`}
       />
     </>
   )
