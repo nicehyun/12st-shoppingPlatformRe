edfddc3d632565126a8da97be283dcd3b6f0079a
ish <lshsh5656@naver.com>
2023-10-31 20:19:41 +0900

    Feat : coupon, mile detail button click ➡️  route / mypage route.ts route 통합

---
+++ src/features/myPage/types/route.ts
@@ -0,0 +1,10 @@
+export enum MyPageRoute {
+  CHECKOUTLIST = "/checkoutList",
+  CHECKOUTCANCELLIST = "/checkoutCancelList",
+  CHECKOUTREVIEWLIST = "/reviewList",
+  WRITEABLEREVIEWS = "/reviewList/writeableReviews",
+  MYREVIEW = "/reviewList/myReview",
+  USERINFOOFMODIFICATION = "/userInfoOfModification",
+  COUPONS = "/coupons",
+  Mile = "/mile",
+}
