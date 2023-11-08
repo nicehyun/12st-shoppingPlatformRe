68de735c99aeaa034b7f316a0734106379440061
ish <lshsh5656@naver.com>
2023-11-08 23:20:05 +0900

    Refactor : MyPageTabs로 수정

--- src/features/myPage/views/shoppingInfo/MyPageCancelList.tsx
+++ src/features/myPage/views/shoppingInfo/MyPageCancelList.tsx
@@ -1,57 +1,31 @@
 "use client"
 
 import MyPageSectionTitle from "../MyPageSectionTitle"
+import MyPageTabs from "../MyPageTabs"
 import MyPageCancelTebPanel from "./MyPageCancelTebPanel"
 import { useTabValueHandler } from "@/features/checkout/hooks/useTabValueHandler"
 import { Box, Tab, Tabs } from "@mui/material"
 
 const MyPageCancelList = () => {
   const { handleTabValueChange, tabValue } = useTabValueHandler()
-
-  const renderTab = () => {
-    const tabs = [
-      { label: "All", id: "all" },
-      { label: "취소", id: "return" },
-      { label: "반품", id: "return" },
-      { label: "교환", id: "change" },
-    ]
-
-    return tabs.map((tab, index) => (
-      <Tab
-        key={`tab-${index}`}
-        label={tab.label}
-        id={`cancel-tab-${tab.id}`}
-        aria-controls={`cancel-tabpanel-${tab.id}`}
-        sx={{
-          "&.Mui-selected": {
-            color: "#ff4e0a",
-          },
-          color: "#ccc",
-          width: "25%",
-        }}
-      />
-    ))
-  }
+  const tabList = [
+    { label: "All", id: "all" },
+    { label: "취소", id: "return" },
+    { label: "반품", id: "return" },
+    { label: "교환", id: "change" },
+  ]
 
   return (
     <section>
       <MyPageSectionTitle title="취소/반품/교환 내역" />
 
-      <Box sx={{ width: "100%", padding: 0 }}>
-        <Box sx={{ borderColor: "#d2d2d2" }}>
-          <Tabs
-            value={tabValue}
-            onChange={handleTabValueChange}
-            aria-label="myPage-cancelList-teps"
-            sx={{
-              "& .MuiTabs-indicator": {
-                backgroundColor: "#ff4e0a",
-              },
-            }}
-          >
-            {renderTab()}
-          </Tabs>
-        </Box>
+      <Box sx={{ width: "100%" }}>
+        <MyPageTabs
+          tabId="cancelList"
+          onChangeTabValue={handleTabValueChange}
+          tabValue={tabValue}
+          tabs={tabList}
+        />
 
         {tabValue === 0 && <MyPageCancelTebPanel cancelType="cancel__all" />}
         {tabValue === 1 && <MyPageCancelTebPanel cancelType="cancel__cancel" />}
--- src/features/myPage/views/shoppingInfo/MyPageReviewList.tsx
+++ src/features/myPage/views/shoppingInfo/MyPageReviewList.tsx
@@ -1,53 +1,29 @@
 "use client"
 
 import MyPageSectionTitle from "../MyPageSectionTitle"
-import { Box, Tab, Tabs } from "@mui/material"
+import { Box } from "@mui/material"
 import MyPageReviewTebPanel from "./MyPageReviewTebPanel"
 import { useTabValueHandler } from "@/features/checkout/hooks/useTabValueHandler"
+import MyPageTabs from "../MyPageTabs"
 
 const MyPageReviewList = () => {
   const { handleTabValueChange, tabValue } = useTabValueHandler()
+  const tabList = [
+    { label: "작성 가능한 리뷰", id: "available" },
+    { label: "내 리뷰 (0)", id: "my" },
+  ]
 
-  const renderTab = () => {
-    const tabs = ["작성 가능한 리뷰", "내 리뷰 (0)"]
-
-    return tabs.map((label, index) => (
-      <Tab
-        key={`tab-review-${index}`}
-        label={label}
-        id={`tab-review__${index === 0 ? "availableReview" : "myReview"}`}
-        aria-controls={`tabpanel-review__${
-          index === 0 ? "availableReview" : "myReview"
-        }`}
-        sx={{
-          "&.Mui-selected": {
-            color: "#ff4e0a",
-          },
-          color: "#ccc",
-          width: "50%",
-        }}
-      />
-    ))
-  }
   return (
     <section>
       <MyPageSectionTitle title="상품 리뷰" />
 
       <Box sx={{ width: "100%", padding: 0 }}>
-        <Box sx={{ borderColor: "#d2d2d2" }}>
-          <Tabs
-            value={tabValue}
-            onChange={handleTabValueChange}
-            aria-label="myPage-useMileAndGetMileInfo-teps"
-            sx={{
-              "& .MuiTabs-indicator": {
-                backgroundColor: "#ff4e0a",
-              },
-            }}
-          >
-            {renderTab()}
-          </Tabs>
-        </Box>
+        <MyPageTabs
+          tabId="reviewList"
+          onChangeTabValue={handleTabValueChange}
+          tabValue={tabValue}
+          tabs={tabList}
+        />
 
         {tabValue === 0 && (
           <MyPageReviewTebPanel reviewType="review__available" />
