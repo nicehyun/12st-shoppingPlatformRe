fc67895c0348dd0d3fabd847f80c4c6b6adabee3
ish <lshsh5656@naver.com>
2023-12-18 22:54:31 +0900

    Refacotor : fetch api로 수정 / json server로 수정

--- src/features/auth/signIn/hooks/useSessionQuery.tsx
+++ src/features/auth/signIn/hooks/useSessionQuery.tsx
@@ -2,7 +2,6 @@ import { useQuery } from "@tanstack/react-query"
 
 import { getSession } from "next-auth/react"
 
-// TODO : 세션 쿼리에 정보 없음.
 const useSessionQuery = () => {
   const {
     data: sessionQuery,
@@ -11,8 +10,6 @@ const useSessionQuery = () => {
   } = useQuery(["session"], () => getSession(), {
     refetchInterval: 30 * 60 * 1000,
   })
-
-  // console.log(sessionQuery)
 
   return { sessionQuery, isSessionCheckError, isSessionCheckLoading }
 }
