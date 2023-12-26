9424ecf71173d1aa67c95a586a9c9166b53f2996
ish <lshsh5656@naver.com>
2023-12-26 15:40:37 +0900

    Feat : 이미지 클릭시 ProductDatail Page로 route

--- src/features/home/views/ShadowProductCard.tsx
+++ src/features/home/views/ShadowProductCard.tsx
@@ -4,6 +4,7 @@ import {
   numberToLocaleString,
 } from "@/features/common/utils/price"
 import Image from "next/image"
+import Link from "next/link"
 
 interface IArrivalProductCard {
   productInfo: Product
@@ -17,14 +18,19 @@ const ShadowProductCard = ({ productInfo }: IArrivalProductCard) => {
   return (
     <div className="m-w-[500px]">
       <div className="overflow-hidden rounded-[8px] mb-[20px] aspect-w-1 aspect-h-1 text-[12px] cardShadow">
-        <Image
-          src={image}
-          alt={`상품사진이 준비되지 않았습니다. - ${name}`}
-          width={0}
-          height={0}
-          sizes="100vw"
-          className="h-full w-full"
-        />
+        <Link
+          href={`/productDetail/${productInfo.id}`}
+          className={`cursor-pointer`}
+        >
+          <Image
+            src={image}
+            alt={`상품사진이 준비되지 않았습니다. - ${name}`}
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="h-full w-full"
+          />
+        </Link>
       </div>
 
       <div className="flex flex-col text-[14px] md:text-[12px]">
