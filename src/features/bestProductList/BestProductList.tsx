e4462fe8a9f938b3976ff9928cf82235750cbb2d
ish <lshsh5656@naver.com>
2023-12-26 15:26:39 +0900

    Refactor : SSG로 수정

--- src/features/bestProductList/BestProductList.tsx
+++ src/features/bestProductList/BestProductList.tsx
@@ -1,10 +1,7 @@
-"use client"
-
 import ProductCard from "../common/views/ProductCard"
-import { usePagination } from "../common/hooks/usePagination"
-import { useGetBestProductListQuery } from "./hooks/useGetBestProductListQuery"
-import Loading from "../common/views/Loading"
-import { Fragment } from "react"
+import { Fragment, use } from "react"
+
+import { bestProductListAPI } from "./models/bestProductListAPI"
 
 interface IBestProductList {
   categoriesPath: string[] | undefined
@@ -12,45 +9,25 @@ interface IBestProductList {
 
 const BestProductList = ({ categoriesPath }: IBestProductList) => {
   const fommatedCategoriesPath = categoriesPath ?? []
-  const { bestProductList, isLoading } = useGetBestProductListQuery(
-    fommatedCategoriesPath
-  )
 
-  const perPage = 12
-  const { listPagination, renderPaginationComponent } = usePagination(
-    perPage,
-    bestProductList.length
-  )
-
-  if (isLoading) {
-    return (
-      <Loading
-        spinnerSize={{ width: "w-[50px]", height: "h-[50px]" }}
-        height="h-[400px]"
-        isFrame={false}
-      />
-    )
-  }
+  const bestProductList =
+    use(
+      bestProductListAPI.getBestProductListWithCategory(fommatedCategoriesPath)
+    ) ?? []
 
   return (
-    <>
-      <div className="grid grid-cols-3 xl:grid-cols-4 md:grid-cols-2 sm:grid-cols-2 gap-[20px] mt-[50px]">
-        {bestProductList
-          .slice(listPagination.indexOfFirst, listPagination.indexOfLast)
-          .map((product, index) => (
-            <Fragment key={`best-product-${product.id}`}>
-              <div className="relative">
-                <ProductCard productInfo={product} />
-                <span className="absolute top-0 left-0 bg-black w-[50px] h-[50px] flexCenter text-white text-[20px] font-bold">
-                  {index + 1}
-                </span>
-              </div>
-            </Fragment>
-          ))}
-      </div>
-
-      <div className="mt-[30px]">{renderPaginationComponent()}</div>
-    </>
+    <div className="grid grid-cols-3 xl:grid-cols-4 md:grid-cols-2 sm:grid-cols-2 gap-[20px] mt-[50px]">
+      {bestProductList.map((product, index) => (
+        <Fragment key={`best-product-${product.id}`}>
+          <div className="relative">
+            <ProductCard productInfo={product} />
+            <span className="absolute top-0 left-0 bg-black w-[50px] h-[50px] flexCenter text-white text-[20px] font-bold">
+              {index + 1}
+            </span>
+          </div>
+        </Fragment>
+      ))}
+    </div>
   )
 }
 
