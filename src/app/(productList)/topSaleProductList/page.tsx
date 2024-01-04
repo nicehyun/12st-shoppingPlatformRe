6fe78b4fa366be08ac20a83e09c7d18c5b79a34a
ish <lshsh5656@naver.com>
2024-01-04 21:59:26 +0900

    Refactor : mateData 수정, prefetch

--- src/app/(productList)/topSaleProductList/page.tsx
+++ src/app/(productList)/topSaleProductList/page.tsx
@@ -1,12 +1,26 @@
+import { homeAPI } from "@/features/home/models/homeAPI"
 import TopSaleProductListSection from "@/features/topSaleProductList/views/TopSaleProductListSection"
+import { getQueryClient } from "@/tanstackQuery/utils/getQueryClient"
+import { Hydrate, dehydrate } from "@tanstack/react-query"
 import { Metadata } from "next"
 
 export const metadata: Metadata = {
-  title: "쇼핑 플랫폼 12ST",
+  title: "12ST",
+  description: "Shopping Platform",
 }
 
-const TopSaleProductListPage = () => {
-  return <TopSaleProductListSection />
+const TopSaleProductListPage = async () => {
+  const queryClient = getQueryClient()
+  await queryClient.prefetchQuery(["indiviualProductList"], () =>
+    homeAPI.getIndividualSectionProductList()
+  )
+  const dehydratedState = dehydrate(queryClient)
+
+  return (
+    <Hydrate state={dehydratedState}>
+      <TopSaleProductListSection />
+    </Hydrate>
+  )
 }
 
 export default TopSaleProductListPage
--- src/features/topSaleProductList/views/SaleProductList.tsx
+++ src/features/topSaleProductList/views/SaleProductList.tsx
@@ -5,6 +5,7 @@ import { useGetIndiviualProductListQuery } from "@/features/home/hooks/useGetInd
 
 const SaleProductList = () => {
   const { topSaleProductList } = useGetIndiviualProductListQuery()
+
   return (
     <div className="grid grid-cols-3 xl:grid-cols-4 md:grid-cols-2 sm:grid-cols-2 gap-[20px] mt-[50px]">
       {topSaleProductList.map((product) => (
