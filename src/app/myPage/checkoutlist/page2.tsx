13887356e380c4cb5ab30f921e1f20662d52b11b
ish <lshsh5656@naver.com>
2024-02-14 16:10:35 +0900

    build

--- src/app/myPage/checkoutlist/page2.tsx
+++
@@ -1,7 +0,0 @@
-import CheckoutListSection from "@/features/myPage/views/shoppingInfo/CheckoutListSection"
-
-const MyPageCheckoutListSection = () => {
-  return <CheckoutListSection />
-}
-
-export default MyPageCheckoutListSection
