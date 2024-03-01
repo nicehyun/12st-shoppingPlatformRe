1987060f5ce91418b78abf8c205b648a6f3c2be9
ish <lshsh5656@naver.com>
2024-03-01 15:06:44 +0900

    node env 설정

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,11 +6,16 @@ export { default } from "next-auth/middleware"
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl
   const secret = process.env.NEXTAUTH_SECRET
+
   const accessToken = await getToken({
     req: request,
     secret: secret,
     // cookieName: "next-auth.session-token",
-    cookieName: "__Secure-next-auth.session-token",
+    // cookieName: "__Secure-next-auth.session-token",
+    cookieName:
+      process.env.NODE_ENV === "production"
+        ? "__Secure-next-auth.session-token"
+        : "next-auth.session-token",
   })
 
   const isCategoryManagementPage = (pathname: string) =>
